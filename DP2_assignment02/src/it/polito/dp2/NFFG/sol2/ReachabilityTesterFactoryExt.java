261f62af444fb79c506e053c9fd89a12bd7e2b39
MicheledG <chiefzephyr@gmail.com>
2016-11-23 16:11:04 +0100

    I don't know how to implement things :'(

---
+++ DP2_assignment02/src/it/polito/dp2/NFFG/sol2/ReachabilityTesterFactoryExt.java
@@ -0,0 +1,15 @@
+package it.polito.dp2.NFFG.sol2;
+
+import it.polito.dp2.NFFG.lab2.ReachabilityTester;
+import it.polito.dp2.NFFG.lab2.ReachabilityTesterException;
+import it.polito.dp2.NFFG.lab2.ReachabilityTesterFactory;
+
+public class ReachabilityTesterFactoryExt extends ReachabilityTesterFactory {
+
+	@Override
+	public ReachabilityTester newReachabilityTester() throws ReachabilityTesterException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ DP2_assignment02/src/it/polito/dp2/NFFG/sol2/ReachabilityTesterImpl.java
@@ -0,0 +1,43 @@
+package it.polito.dp2.NFFG.sol2;
+
+import it.polito.dp2.NFFG.*;
+import it.polito.dp2.NFFG.lab2.NoGraphException;
+import it.polito.dp2.NFFG.lab2.ReachabilityTester;
+import it.polito.dp2.NFFG.lab2.ServiceException;
+import it.polito.dp2.NFFG.lab2.UnknownNameException;
+
+public class ReachabilityTesterImpl implements ReachabilityTester {
+	
+	//TODO: HOW WE HAVE TO RETRIEVE RANDOM DATA?
+	NffgVerifier nffgVerifier;
+	
+	public ReachabilityTesterImpl() {
+		try {
+			//TODO: WHICH IS THE "NffgVerifierFactory" TO USE?
+			this.nffgVerifier = NffgVerifierFactory.newInstance().newNffgVerifier();
+		} catch (NffgVerifierException | FactoryConfigurationError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void loadNFFG(String name) throws UnknownNameException, ServiceException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean testReachability(String srcName, String destName)
+			throws UnknownNameException, ServiceException, NoGraphException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getCurrentGraphName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
