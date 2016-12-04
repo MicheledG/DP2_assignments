e21a4ea752b2edd9d8305fe9174789790d04bad6
MicheledG <chiefzephyr@gmail.com>
2016-12-04 17:58:57 +0100

    ReachabilityTesterImpl implemented for loadNFFG and getUpdatedGraph

--- DP2_assignment02/src/it/polito/dp2/NFFG/sol2/ReachabilityTesterImpl.java
+++ DP2_assignment02/src/it/polito/dp2/NFFG/sol2/ReachabilityTesterImpl.java
@@ -1,14 +1,11 @@
 package it.polito.dp2.NFFG.sol2;
 
 import java.net.URI;
+import java.util.ArrayList;
 
-import javax.ws.rs.ClientErrorException;
-import javax.ws.rs.RedirectionException;
-import javax.ws.rs.ServerErrorException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.ResponseProcessingException;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.client.Entity;
 
 import it.polito.dp2.NFFG.*;
 import it.polito.dp2.NFFG.Random.NffgVerifierFactoryImpl;
@@ -21,8 +18,10 @@ import it.polito.dp2.NFFG.lab2.UnknownNameException;
 
 public class ReachabilityTesterImpl implements ReachabilityTester {
 	
+	
+	URI baseURL = null;
 	NffgVerifier nffgVerifier;
-	URI baseURL;
+	String uploadedNffgName = null;
 	
 	public ReachabilityTesterImpl() {
 		try {
@@ -33,7 +32,11 @@ public class ReachabilityTesterImpl implements ReachabilityTester {
 			System.err.println("Exception instantiating a new NffgVerifier");
 			e.printStackTrace();
 		}
-		baseURL = URI.create(System.getProperty("it.polito.dp2.NFFG.lab2.URL"));
+		String baseURL = System.getProperty("it.polito.dp2.NFFG.lab2.URL");
+		if(baseURL == null)
+			this.baseURL = URI.create("http://localhost:8080/Neo4JXML/rest/");
+		else
+			this.baseURL = URI.create(baseURL);
 	}
 	
 	@Override
@@ -43,13 +46,26 @@ public class ReachabilityTesterImpl implements ReachabilityTester {
 		if(nffgReader == null)
 			throw new UnknownNameException();
 		/* Delete all the eventual nodes on the Neo4JXML DB */
+		//TODO: debug
+		System.out.println("Deleting...");
 		deleteNFFG();
-		/* Load all the nodes of the Nffg named "name" */
+		/* Load all the nodes of the Nffg */
+		//TODO: debug
+		System.out.println("Uploading...");
 		for (NodeReader nodeReader: nffgReader.getNodes()) {
+			//TODO: debug
+			System.out.println("Uploading node "+nodeReader.getName()+"...");
 			loadNode(nodeReader);
+			/* Load all the relationships of the node */
+			for (LinkReader linkReader : nodeReader.getLinks()) {
+				//TODO: debug
+				System.out.println("Uploading relationship "+linkReader.getName()+"...");
+				loadRelationship(linkReader);
+			}
 		}
 		
-		
+		/* last thing: if the loading ends up successfully set uploadedNffgName */
+		uploadedNffgName = name;
 	}
 
 	@Override
@@ -61,36 +77,67 @@ public class ReachabilityTesterImpl implements ReachabilityTester {
 
 	@Override
 	public String getCurrentGraphName() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.uploadedNffgName;
 	}
 	
 	/* Delete all the eventual nodes on the Neo4JXML DB */
 	private void deleteNFFG() throws ServiceException {
 		Client client = ClientBuilder.newClient();
 		try{
-			client.target(this.baseURL+"/resource/nodes").request("text/plain").get(String.class);
+			client.target(this.baseURL+"resource/nodes").request("application/xml").delete();
 		}
 		catch (RuntimeException e) {
 			throw new ServiceException(e);	
 		}
-		
+		this.uploadedNffgName = null;
 	}
 	
-	/* Load a node with all its links */
+	/* Load a node */
 	private void loadNode(NodeReader nodeReader) throws ServiceException{
-		/* load the new node resource*/
-		//TODO: PROBLEM DETECTING THE RIGHT NODE CLASS => I WOULD LIKE TO USE THE GEN-SRC ONE
-		Node node = NodeReaderToNode(nodeReader);
-		/* load all the node links as relationships */
-		
+		/* create a node object to be loaded*/
+		Node node = newNode(nodeReader);
+		/* load the node through API*/
+		Client client = ClientBuilder.newClient();
+		try{
+			client.target(this.baseURL+"resource/node").request("application/xml").post(Entity.xml(node));
+		}
+		catch (RuntimeException e) {
+			throw new ServiceException(e);	
+		}
 	}
 	
-	private Node NodeReaderToNode(NodeReader nodeReader){
+	private Node newNode(NodeReader nodeReader){
 		Node node = new Node();
-		node.id = nodeReader.getName();
-		//TODO: complete the translation
+		node.setId(nodeReader.getName());
+		Property name = new Property();
+		name.setName("name");
+		name.setValue(nodeReader.getName());
+		node.getProperty().add(name);
 		return node;
+	}
+	
+	/* Load a relationship */
+	private void loadRelationship(LinkReader linkReader) throws ServiceException{
+		/* create a relationship object to be loaded*/
+		Relationship relationship = newRelationship(linkReader);
+		/* load the relationship through API*/
+		Client client = ClientBuilder.newClient();
+		try{
+			String nodeId = relationship.srcNode;
+			client.target(this.baseURL+"/resource/node/"+nodeId+"/relationship").request("application/xml").post(Entity.xml(relationship));
+		}
+		catch (RuntimeException e) {
+			throw new ServiceException(e);	
+		}
+	}
+	
+	private Relationship newRelationship(LinkReader linkReader){
+		Relationship relationship = new Relationship();
+		relationship.setId(linkReader.getName());
+		relationship.setType("Link");
+		relationship.setSrcNode(linkReader.getSourceNode().getName());
+		relationship.setDstNode(linkReader.getDestinationNode().getName());
+		return relationship;
 	}
 	
 	//TODO:debug
@@ -105,7 +152,23 @@ public class ReachabilityTesterImpl implements ReachabilityTester {
 		}
 		
 		try {
-			reachabilityTester.loadNFFG("Nffg0");
+			System.out.println("Looking for an eventual nffg already updated...");
+			String nffgName = reachabilityTester.getCurrentGraphName();
+			if(nffgName == null)
+				System.out.println("No updated nffg");
+			else
+				System.out.println("Nffg updated: "+nffgName);
+			for(Integer i = 0; i < 10; i++ ){
+				nffgName = "Nffg" + i.toString();
+				System.out.println("Deleting previous updated nffg and uploading "+nffgName+"...");
+				reachabilityTester.loadNFFG(nffgName);
+				nffgName = reachabilityTester.getCurrentGraphName();
+				if(nffgName == null)
+					System.out.println("No updated nffg");
+				else
+					System.out.println("Nffg updated: "+nffgName);
+			}
+			
 		} catch (UnknownNameException | ServiceException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
