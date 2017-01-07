3159a6116030765f66f9c143f960168e2c735347
MicheledG <chiefzephyr@gmail.com>
2017-01-07 18:46:58 +0100

    Working on DP2_assignemnt03b
    First hellone versione developed

--- DP2_assignment03b/src/it/polito/dp2/NFFG/sol3/service/NffgService.java
+++ DP2_assignment03b/src/it/polito/dp2/NFFG/sol3/service/NffgService.java
@@ -1,5 +1,24 @@
 package it.polito.dp2.NFFG.sol3.service;
 
-public class NffgService {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
+@Path("/")
+public class NffgService {
+	
+	@Path("hellone")
+	@GET
+	@Produces("text/plain")
+	public String hellone(){
+		return new String("hellone");
+	}
+	
+	@Path("ciaone")
+	@GET
+	@Produces("text/plain")
+	public String ciaone(){
+		return new String("ciaone");
+	}
+	
 }
