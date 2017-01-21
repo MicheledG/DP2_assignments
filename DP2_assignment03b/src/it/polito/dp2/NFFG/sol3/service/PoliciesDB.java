76a2f87f3e79806310b6058ada242c309dbb2146
MicheledG <chiefzephyr@gmail.com>
2017-01-21 12:19:31 +0100

    Working on DP2_assignment03b
    PoliciesDB implemented in ThreadSafe version

--- DP2_assignment03b/src/it/polito/dp2/NFFG/sol3/service/PoliciesDB.java
+++ DP2_assignment03b/src/it/polito/dp2/NFFG/sol3/service/PoliciesDB.java
@@ -1,28 +1,19 @@
 package it.polito.dp2.NFFG.sol3.service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import it.polito.dp2.NFFG.sol3.service.exceptions.*;
 import it.polito.dp2.NFFG.sol3.service.jaxb.Policies;
 
 public class PoliciesDB {
 	
-	private Map<String, Policies.Policy> mapPolicyNamePolicyObject = new HashMap<String, Policies.Policy>();
+	private Map<String, Policies.Policy> mapPolicyNamePolicyObject = new ConcurrentHashMap<String, Policies.Policy>();
 	
 	private static PoliciesDB policiesDB = new PoliciesDB();
-	
-	private void updatePolicyEntry(String policyName, Policies.Policy policyUpdate) {
-		
-		/* remove the old policy and add the new one*/
-		this.mapPolicyNamePolicyObject.remove(policyName);
-		this.mapPolicyNamePolicyObject.put(policyName, policyUpdate);
-		
-	}
-	
 	
 	public static PoliciesDB newPoliciesDB(){
 		return policiesDB;
@@ -31,29 +22,26 @@ public class PoliciesDB {
 	/* store or update the policies (1 or more) into the DB*/
 	public void storePolicy(Policies.Policy policy){
 		String policyName = policy.getName();
-		if(this.containsPolicy(policyName)) {
-			/* it is an update of a policy */
-			this.updatePolicyEntry(policyName, policy);
-		}
-		else
-			/* it is a new policy to store */
-			this.mapPolicyNamePolicyObject.put(policyName, policy);			
+		/* also if there is already a policy the put works correctly*/
+		this.mapPolicyNamePolicyObject.put(policyName, policy);
 	}
 
-	/* return the list of all the nffg */
+	/* return the list of all the policy names */
 	public Set<String> getPolicyNames() throws NoPolicyException {
-		if(this.mapPolicyNamePolicyObject.isEmpty())
+		Set<String> policyNames = this.mapPolicyNamePolicyObject.keySet();
+		if(policyNames.isEmpty())
 			throw new NoPolicyException("no policy in the DB");
 
-		return this.mapPolicyNamePolicyObject.keySet();
+		return policyNames;
 	}
 	
-	/* return policies containing a single nffg */
+	/* return policy */
 	public Policies.Policy getPolicy(String policyName) throws UnknownNameException{
-		if(!this.mapPolicyNamePolicyObject.containsKey(policyName))
+		Policies.Policy policy = this.mapPolicyNamePolicyObject.get(policyName);
+		if(policy == null)
 			throw new UnknownNameException("policy named " + policyName + " not found");
 		else
-			return this.mapPolicyNamePolicyObject.get(policyName);	
+			return policy;	
 	}
 	
 	/* clear local map */
@@ -63,12 +51,9 @@ public class PoliciesDB {
 	
 	/* remove policy from the local map */
 	public void deletePolicy(String policyName) throws UnknownNameException{		
-		
-		if(!this.mapPolicyNamePolicyObject.containsKey(policyName))
+		Policies.Policy policyRemoved = this.mapPolicyNamePolicyObject.remove(policyName);
+		if(policyRemoved == null)
 			throw new UnknownNameException("policy named " + policyName + " not found");
-		else
-			this.mapPolicyNamePolicyObject.remove(policyName);
-	
 	}
 	
 	public boolean containsPolicy(String policyName){
@@ -80,20 +65,22 @@ public class PoliciesDB {
 	}
 	
 	/* check if there is a policy referring a particular nffg */
+	/* using concurrent hash map there are no problem during iteration => no concurrent modification exception (and also no semantic problem) */
 	public boolean areReferringThisNffg(String nffgName){
-		for (Map.Entry<String, Policies.Policy> mapEntry: this.mapPolicyNamePolicyObject.entrySet()) {
-			if(mapEntry.getValue().getNffg().equals(nffgName))
+		for (Map.Entry<String, Policies.Policy> entryPolicyNamePolicyObjectEntry: this.mapPolicyNamePolicyObject.entrySet()) {
+			if(entryPolicyNamePolicyObjectEntry.getValue().getNffg().equals(nffgName))
 				return true;
 		}
 		return false;
 	}
 	
 	/* return the list of the policy names which refers a Nffg */
+	/* using concurrent hash map there are no problem during iteration => no concurrent modification exception (and also no semantic problem) */
 	public List<String> getPolicyNamesReferringNffg(String nffgName){
 		List<String> policyNames = new ArrayList<String>();
-		for (Map.Entry<String, Policies.Policy> mapPolicyNamePolicyObjectEntry: this.mapPolicyNamePolicyObject.entrySet()) {
-			if(mapPolicyNamePolicyObjectEntry.getValue().getNffg().equals(nffgName))
-				policyNames.add(mapPolicyNamePolicyObjectEntry.getKey());
+		for (Map.Entry<String, Policies.Policy> entryPolicyNamePolicyObjectEntry: this.mapPolicyNamePolicyObject.entrySet()) {
+			if(entryPolicyNamePolicyObjectEntry.getValue().getNffg().equals(nffgName))
+				policyNames.add(entryPolicyNamePolicyObjectEntry.getKey());
 		}
 		return policyNames;
 	}
@@ -101,5 +88,5 @@ public class PoliciesDB {
 	public boolean isEmpty(){
 		return this.mapPolicyNamePolicyObject.isEmpty();
 	}
-
+	
 }
